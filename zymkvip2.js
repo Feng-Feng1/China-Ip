25822ebdb7203e8cc544fef5e4445e44e446d94a
Feng-Feng1 <145860797+Feng-Feng1@users.noreply.github.com>
2025-10-31 16:06:32 +0800

    Update and rename zymkvip2 to zymkvip2.js

--- zymkvip2.js
+++ zymkvip2.js
@@ -3,9 +3,9 @@
 #!update=2025-10-27
 
 [Script]
-http-response ^https?:\/\/apigate\.kaimanhua\.com\/zymk.+getuserinfo requires-body=true, script-path=https://raw.githubusercontent.com/Feng-Feng1/surge/refs/heads/main/zymkvip2, tag=知音漫客-会员解锁, timeout=10
+http-response ^https?:\/\/apigate\.kaimanhua\.com\/zymk.+getuserinfo requires-body=true, script-path=https://raw.githubusercontent.com/Feng-Feng1/surge/refs/heads/main/zymkvip2.js, tag=知音漫客-会员解锁, timeout=10
 
-http-response ^https?:\/\/apigate\.kaimanhua\.com\/zymk.+paychapters requires-body=true, script-path=https://raw.githubusercontent.com/Feng-Feng1/surge/refs/heads/main/zymkvip2, tag=知音漫客-章节解锁, timeout=10
+http-response ^https?:\/\/apigate\.kaimanhua\.com\/zymk.+paychapters requires-body=true, script-path=https://raw.githubusercontent.com/Feng-Feng1/surge/refs/heads/main/zymkvip2.js, tag=知音漫客-章节解锁, timeout=10
 
 http-response ^https?://api-cdn\.kaimanhua\.com/advertiseapi/app/advertise/getappadvertise reject
 
